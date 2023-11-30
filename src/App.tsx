7a31fe1a9b6ec5c3d6be1189beab06bf4d70f4a6
lucas <lucaspio.galvao@gmail.com>
2023-11-30 00:44:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+    return (
+        <>
+            <header className="nav-bar">
+                <img src="/app-logo.png" alt="ecommerce-logo" className="app-logo"/>
+                <div className="links">
+                    <p>Home</p>
+                    <p>Meu carrinho</p>
+                    <p>Contate-nos</p>
+                </div>
+            </header>
+            <div className="main-container"></div>
+            <footer></footer>
+        </>
+    )
+}
